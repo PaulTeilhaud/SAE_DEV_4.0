a91089929a083ec661cd922022ec5e1e5f691b6b
GabinLeveugle <Gabin.Leveugle@etu.univ-savoie.fr>
2023-01-04 17:44:53 +0100

    scene

--- Project1/Project1/Game1.cs
+++ Project1/Project1/Game1.cs
@@ -1,23 +1,31 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
 
 namespace Project1
 {
     public class Game1 : Game
     {
+        private readonly ScreenManager _screenManager;
+        public SpriteBatch SpriteBatch { get; set; }
+        Acceuil _acceuil;
+        Jeu _jeu;
+        Fin _fin;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private TiledMap _tiledMap;
-        private TiledMapRenderer _tiledMapRenderer;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            _screenManager = new ScreenManager();
+            Components.Add(_screenManager);
         }
 
         protected override void Initialize()
@@ -29,9 +37,10 @@ namespace Project1
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _tiledMap = Content.Load<TiledMap>("MAP_Labyrinthe2");
-            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            _acceuil = new Acceuil(this); // en leur donnant une référence au Game
+            _jeu = new Jeu(this);
+            _fin = new Fin(this);
+            
 
             // TODO: use this.Content to load your game content here
         }
@@ -40,17 +49,36 @@ namespace Project1
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            _tiledMapRenderer.Update(gameTime);
+            {
+                GraphicsDevice.BlendState = BlendState.AlphaBlend;
+                KeyboardState keyboardState = Keyboard.GetState();
+                
+                if (keyboardState.IsKeyDown(Keys.A))
+                {
+                    _screenManager.LoadScreen(_acceuil, new FadeTransition(GraphicsDevice,
+                    Color.Black));
+                }
+                
+               
+                
 
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
+                else if (keyboardState.IsKeyDown(Keys.E))
+                    {
+                        _screenManager.LoadScreen(_jeu, new FadeTransition(GraphicsDevice, Color.Black));
+                    }
+                else if (keyboardState.IsKeyDown(Keys.R))
+                 {
+                        _screenManager.LoadScreen(_fin, new FadeTransition(GraphicsDevice,
+                        Color.Black));
+                }
+                }
+                base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            _tiledMapRenderer.Draw();
+            
 
             // TODO: Add your drawing code here
 
